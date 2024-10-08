2ca4c5bf3454ae225ef07fee52ef96a655aba4a9
Bruno Brilhante <brunobrilhante53@gmail.com>
2024-10-08 17:57:51 -0300

    Estado com arrays

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
   state = {
-      name: 'Bruno',
-      counter: 0
-    };
-
-  handlePClick = () => {
-    this.setState({ name: 'Júnior' });
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;    
-    this.setState({ counter: counter + 1 });
-  }
+    posts: [
+      {
+        id: 1,
+        title: 'Titulo 1',
+        body: 'Corpo 1'
+      },
+      {
+        id: 2,
+        title: 'Titulo 2',
+        body: 'Corpo 2'
+      },
+      {
+        id: 3,
+        title: 'Titulo 3',
+        body: 'Corpo 3'
+      },
+    ],
+  };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
